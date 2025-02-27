997cc984afa6a18d07cf38e1a6f3d0b4e97c9254
Teddychuks <chukwukaohuruogu@gmail.com>
2025-02-27 08:38:29 +0100

    configService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,15 +7,20 @@ import { TodoModule } from './todo/todo.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.MYSQLHOST,
-      port:  parseInt(process.env.MYSQLPORT || '3306'), 
-      username: process.env.MYSQLUSER,
-      password: process.env.MYSQLPASSWORD,
-      database: process.env.MYSQLDATABASE,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('MYSQLHOST'),
+        port: configService.get<number>('MYSQLPORT') || 3306,
+        username: configService.get<string>('MYSQLUSER'),
+        password: configService.get<string>('MYSQLPASSWORD'),
+        database: configService.get<string>('MYSQLDATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
     }),
     TodoModule,
   ],
