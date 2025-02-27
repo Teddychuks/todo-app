1fce7f8f8ca1109934915666034330cd52b6595c
Teddychuks <chukwukaohuruogu@gmail.com>
2025-02-27 13:59:37 +0100

    localhost

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,24 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { join } from 'path';
 
 async function bootstrap() {
- 
-  const PORT = process.env.PORT ?? 3000;
-  const app = await NestFactory.create(AppModule);
-  app.enableCors(); 
-  await app.listen(PORT);
+  const HTTP_PORT = process.env.HTTP_PORT || 3000;  
+  const GRPC_PORT = process.env.GRPC_PORT || 50052; 
 
+  // Create REST API server
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  await app.listen(HTTP_PORT);
+
+  // Create gRPC microservice
   const grpcApp = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
     transport: Transport.GRPC,
     options: {
       package: 'todo',
       protoPath: join(__dirname, '../../proto/todo.proto'),
-      url: `localhost:${PORT}`,
+      url: `localhost:${GRPC_PORT}`, 
     },
   });
 
   await grpcApp.listen();
-  
 }
 bootstrap();
